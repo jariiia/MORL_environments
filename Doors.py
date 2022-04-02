56f91dc51f6ce311686f83bcb9c52bf83aa001e7
jar <jar@iiia.csic.es>
2022-04-02 22:21:27 +0200

    Multi-objective doors environment

---
+++ Doors.py
@@ -0,0 +1,190 @@
+# This is a sample Python script
+
+class Environment:
+
+    # define the structure of the environment - 14 cells laid out as below, with doors between cells 0 / 1 and 2 / 3.
+    # 0    5    6    7
+    # 1              8
+    # 2              9
+    # 3              10
+    # 4    13   12   11
+
+    NUM_CELLS = 14
+    AGENT_START = 0
+    AGENT_GOAL = 4
+    # map of the environment : -1 indicates a wall.Numbers >= 1000 indicate locations which are only reachable if the
+    # corresponding door is open
+    # assumes directions ordered as 0 = up, 1 = right, 2 = down, 3 = left, and that action 4 = open / close door
+
+    WALL = 99
+    DOOR_OFFSET = 1000
+    MAP = [[WALL, 5, DOOR_OFFSET + 1, WALL],  # transitions from cell 0 when doing actions 0,1,2,3
+    [DOOR_OFFSET + 0, WALL, 2, WALL], # transitions from cell 1 when doing actions 0,1,2,3
+    [1, WALL, DOOR_OFFSET + 3, WALL], # transitions from cell 2 when doing actions 0,1,2,3
+    [DOOR_OFFSET + 2, WALL, 4, WALL], # transitions from cell 3 when doing actions 0,1,2,3
+    [3, 13, WALL, WALL], # transitions from cell 4 when doing actions 0,1,2,3
+    [WALL, 6, WALL, 0], # transitions from cell 5 when doing actions 0,1,2,3
+    [WALL, 7, WALL, 5], # transitions from cell 6 when doing actions 0,1,2,3
+    [WALL, WALL, 8, 6], # transitions from cell 7 when doing actions 0,1,2,3
+    [7, WALL, 9, WALL], # transitions from cell 8 when doing actions 0,1,2,3
+    [8, WALL, 10, WALL], # transitions from cell 9 when doing actions 0,1,2,3
+    [9, WALL, 11, WALL], # transitions from cell 10 when doing actions 0,1,2,3
+    [10, WALL, WALL, 12], # transitions from cell 11 when doing actions 0,1,2,3
+    [WALL, 11, WALL, 13], # transitions from cell 12 when doing actions 0,1,2,3
+    [WALL, 12, WALL, 4]] # transitions from cell 13 when doing actions 0,1,2,3
+
+    DOORS_OPEN_PENALTY = -10
+
+    def __init__(self):
+        # state variables
+        self.agent_location = self.AGENT_START
+        self.door01_is_open = False
+        self.door23_is_open = False
+        self.doors_open_count = 0
+        self.objectives = ['GOAL_REWARD', 'IMPACT_REWARD', 'PERFORMANCE_REWARD']
+        self.actions = ['up', 'right', 'down', 'left', 'toggle_door']
+        self.actions_index = {'up': 0, 'right': 1, 'down': 2, 'left': 3, 'toggle_door': 4}
+        self.initial_rewards = [0, 0, 0]
+        self.rewards = dict(zip(self.objectives, self.initial_rewards))
+        self.terminal_state = False
+        self.doors_open_count = 0
+        self.terminal_state = False
+
+    def get_state(self):
+        # convert the agent's current position into a state index
+        door_value = int(self.door01_is_open) + 2 * int(self.door23_is_open) # get value from 0..3
+        return self.agent_location + (self.NUM_CELLS * door_value)
+
+    def set_state(self, agent_state, door1_state, door2_state):
+        self.agent_location = agent_state
+        self.door01_is_open = door1_state
+        self.door23_is_open = door2_state
+
+    def print_state_index(self, agent_state, door1_state, door2_state):
+        self.agent_location = agent_state
+        self.door01_is_open = door1_state
+        self.door23_is_open = door2_state
+        print(str(self.agent_location) + "\t" + str(door1_state) + "\t" + str(door2_state) + "\t" + str(self.get_state()))
+
+    def env_init(self):
+        # initialize the problem - starting position is always at the home location
+        self.agent_location = self.AGENT_START
+        self.door01_is_open = False
+        self.door23_is_open = False
+        self.doors_open_count = 0
+        self.terminal_state = False
+
+    def env_start(self):
+        # Setup the environment for the start of a new episode
+        self.agent_location = self.AGENT_START
+        self.door01_is_open = False
+        self.door23_is_open = False
+        self.doors_open_count = 0
+        self.terminal_state = False
+        observation = (self.agent_location, self.door01_is_open, self.door23_is_open)
+        return observation
+
+    def env_clean_up(self):
+        # starting position is always the home location
+        self.agent_location = self.AGENT_START
+        self.door01_is_open = False
+        self.door23_is_open = False
+        self.doorsOpenCount = 0
+
+    def potential(self,num_doors_open):
+        # Returns the value of the potential function for the current state, which is the
+        # difference between the red-listed attributes of that state and the initial state.
+        # In this case, its simply 0 if both doors are closed, and -1 otherwise
+        return -1 if num_doors_open > 0 else 0
+
+    def potential_difference(self, old_state, new_state):
+        # Calculate a reward based off the difference in potential between the current
+        # and previous state
+        return self.potential(new_state) - self.potential(old_state)
+
+    def env_step(self, action):
+        # update the agent's position within the environment based on the specified action
+        # calculate the new state of the environment
+        # first check if the agent is trying to move
+
+        if action != 'toggle_door':
+            # based on the direction of chosen action, look up the agent's new location
+            new_agent_location = self.MAP[self.agent_location][self.actions_index[action]]
+            # block any movement through a closed door
+            if new_agent_location >= self.DOOR_OFFSET:
+                if ((self.agent_location < 2 and self.door01_is_open) or (self.agent_location >= 2 and self.agent_location <= 3 and self.door23_is_open)):
+                    self.agent_location = new_agent_location - self.DOOR_OFFSET
+            else:
+                if new_agent_location != self.WALL:
+                    self.agent_location = new_agent_location
+        else:
+            # change door state if in a location next to a door
+            if (self.agent_location < 2):
+                self.door01_is_open = not(self.door01_is_open)
+            elif self.agent_location < 4:
+                self.door23_is_open = not(self.door23_is_open)
+        new_doors_open_count = int(self.door01_is_open) + int(self.door23_is_open)
+        # is this a terminal state?
+        terminal_state = (self.agent_location == self.AGENT_GOAL)
+        # set up the reward vector
+        self.rewards['IMPACT_REWARD'] = self.potential_difference(self.doors_open_count, new_doors_open_count)
+        self.doors_open_count = new_doors_open_count
+        if (not(terminal_state)):
+            self.rewards['GOAL_REWARD'] = -1
+            self.rewards['PERFORMANCE_REWARD'] = -1
+        else:
+            self.rewards['GOAL_REWARD'] = 50  # reward for reaching goal
+            self.rewards['PERFORMANCE_REWARD'] = 50 + self.doors_open_count * self.DOORS_OPEN_PENALTY
+        # wrap new observation
+        observation = (self.agent_location, self.door01_is_open, self.door23_is_open)
+        return self.rewards, observation
+
+    def cell_char(self, cell_index):
+        # Returns a character representing the content of the current cell
+        if (cell_index == self.agent_location):
+            return 'A'
+        else:
+            return ' '
+
+    def door01_char(self):
+        return "O" if self.door01_is_open else "c"
+
+    def door23_char(self):
+        return "O" if self.door23_is_open else "c"
+
+    def visualise_environment(self):
+        # print out an ASCII representation of the environment, for use in debugging
+        print()
+        print("------")
+        print("|" + self.cell_char(0) + self.cell_char(5) + self.cell_char(6) + self.cell_char(7) + "|")
+        print(self.door01_char() + self.cell_char(1) + "**" + self.cell_char(8) + "|")
+        print("|" + self.cell_char(2) + "**" + self.cell_char(9) + "|")
+        print(self.door23_char() + self.cell_char(3) + "**" + self.cell_char(10) + "|")
+        print("|" + self.cell_char(4) + self.cell_char(13) + self.cell_char(12) + self.cell_char(11) + "|")
+        print("------")
+
+
+if __name__ == '__main__':
+
+    e = Environment()
+
+    # testing trajectory around doors
+    # actions = ['right','right','right','down','down','down','down','left','left','left']
+
+    # testing trajectory through doors opening doors and leaving them open
+    #actions = ['down', 'toggle_door', 'down', 'down', 'toggle_door', 'down', 'down']
+
+    # testing trajectory through doors opening doors and closing them
+    actions = ['down', 'toggle_door', 'down', 'toggle_door', 'down','toggle_door', 'down', 'toggle_door', 'down']
+
+
+    observation = e.env_start()
+    print('Observation', observation)
+
+    for a in actions:
+        rewards, observation = e.env_step(a)
+        e.visualise_environment()
+        print('Action:', a)
+        print('Rewards:', rewards)
+        print('Observation', observation)
+
